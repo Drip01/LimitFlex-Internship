b0ade47210c43b90fc8831724bfc628cf41e85a2
Ifeanyi <maduabuchimarvelous@gmail.com>
2025-04-24 15:59:17 +0100

    Minimum Element

---
+++ src/internship/week5/algorithm/MinimumElementChallenge.java
@@ -0,0 +1,43 @@
+package internship.week5.algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinimumElementChallenge {
+
+    public static void main(String[] args) {
+
+        int[] returnedArray = readIntegers();
+        System.out.println(Arrays.toString(returnedArray));
+
+        int returnedMin = findMin(returnedArray);
+        System.out.println("min = " + returnedMin);
+    }
+
+    private static int[] readIntegers() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a list of integers, separated by commas:");
+        String input = scanner.nextLine();
+
+        String[] splits = input.split(",");
+        int[] values = new int [splits.length];
+
+        for (int i = 0; i < splits.length; i++) {
+            values[i] = Integer.parseInt(splits[i].trim());
+        }
+
+        return values;
+    }
+
+    private static int findMin(int[] array) {
+
+        int min = Integer.MAX_VALUE;
+        for (int drip : array) {
+            if (drip < min) {
+                min = drip;
+            }
+        }
+        return min;
+    }
+}
