2eac60b6e18c80c7ff5d803ee12aa4e4fbd85f61
Ifeanyi <maduabuchimarvelous@gmail.com>
2025-04-28 11:46:11 +0100

    Mutiple Implementation & Real World Example

--- src/internship/week6/Animal1.java
+++ src/internship/week6/Animal1.java
@@ -2,6 +2,9 @@ package internship.week6;
 
  interface FlightEnabled {
 
+     double MILES_TO_KM = 1.60934;
+     double KM_TO_MILES = 0.621371;
+
       void takeoff();
      void land();
      void fly();
---
+++ src/internship/week6/Jet.java
@@ -0,0 +1,27 @@
+package internship.week6;
+
+public class Jet implements FlightEnabled, Trackable {
+
+    @Override
+    public void takeoff() {
+        System.out.println(getClass().getSimpleName() + " is takeoff");
+    }
+
+    @Override
+    public void land() {
+        System.out.println(getClass().getSimpleName() + " is landing");
+
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getClass().getSimpleName() + " is flying");
+
+    }
+
+    @Override
+    public void track() {
+        System.out.println(getClass().getSimpleName() + "'s coordinates recorded");
+
+    }
+}
