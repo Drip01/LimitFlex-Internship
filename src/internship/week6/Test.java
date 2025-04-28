eaf115ce268ea5555d2c96fa153dd6ac1b4be75a
Ifeanyi <maduabuchimarvelous@gmail.com>
2025-04-28 12:57:35 +0100

    Enhancement: Private & Static Methods in Action

--- src/internship/week6/Test.java
+++ src/internship/week6/Test.java
@@ -5,6 +5,10 @@ public class Test {
     public static void main(String[] args) {
 
         inFlight(new Jet());
+
+//        OrbitEarth.log("Testing " + new Satellite());
+
+        orbit(new Satellite());
     }
 
     private static void inFlight(FlightEnabled flier) {
@@ -18,4 +22,10 @@ public class Test {
         flier.land();
     }
 
+    private static void orbit (OrbitEarth flier) {
+
+        flier.takeoff();
+        flier.fly();
+        flier.land();
+        }
 }
