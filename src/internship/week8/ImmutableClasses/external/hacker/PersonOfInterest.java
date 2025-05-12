e3dd3213b3ce80768b7c3f964f058a64dae7c330
Ifeanyi <maduabuchimarvelous@gmail.com>
2025-05-12 10:32:10 +0100

    Immutable Java Classes: Preventing Subclass Modification

---
+++ src/internship/week8/ImmutableClasses/external/hacker/PersonOfInterest.java
@@ -0,0 +1,11 @@
+package internship.week8.ImmutableClasses.external.hacker;
+
+import internship.week8.ImmutableClasses.PersonImmutable;
+
+public class PersonOfInterest extends PersonImmutable {
+
+    public PersonOfInterest(PersonImmutable person) {
+        super(person);
+    }
+
+}
