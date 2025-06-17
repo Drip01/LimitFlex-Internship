52296d3214f346af92d2ee0aa70e02d9da35b95d
Ifeanyi <maduabuchimarvelous@gmail.com>
2025-06-17 10:33:10 +0100

    Assertions: Essential Techniques for JUnit Testing

--- src/internship/week13/Banking/BankAccountTest.java
+++ src/internship/week13/Banking/BankAccountTest.java
@@ -1,6 +1,6 @@
 package internship.week13.Banking;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -8,7 +8,9 @@ import static org.junit.Assert.fail;
 public class BankAccountTest {
 	@org.junit.Test
 	public void deposit() throws Exception {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Mr", "Drip", 1000.00, BankAccount.CHECKING);
+		double balance = account.deposit(200.00, true);
+		assertEquals(1200.00, balance, 0);
 	}
 
 	@org.junit.Test
@@ -17,8 +19,22 @@ public class BankAccountTest {
 	}
 
 	@org.junit.Test
-	public void getBalance() throws Exception {
-		fail("This test has yet to be implemented");
+	public void getBalance_deposit() throws Exception {
+		BankAccount account = new BankAccount("Mr", "Drip", 1000.00, BankAccount.CHECKING);
+		account.deposit(200.00, true);
+		assertEquals(1200.00, account.getBalance(), 0);
 	}
 
+	@org.junit.Test
+	public void getBalance_withdraw() throws Exception {
+		BankAccount account = new BankAccount("Mr", "Drip", 1000.00, BankAccount.CHECKING);
+		account.withdraw(200.00, true);
+		assertEquals(800.00, account.getBalance(), 0);
+	}
+
+	@org.junit.Test
+	public void isChecking_true() {
+		BankAccount account = new BankAccount("Mr", "Drip", 1000.00, BankAccount.CHECKING);
+		assertTrue("The account is NOT a checking account", account.isChecking());
+	}
 }
