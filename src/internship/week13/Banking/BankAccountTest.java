b559b1710bc1e5cf6defb27d6bcd9bb87a293c90
Ifeanyi <maduabuchimarvelous@gmail.com>
2025-06-17 09:54:51 +0100

    Mastering Unit Testing Essentials & JUnit Framework in Java

---
+++ src/internship/week13/Banking/BankAccountTest.java
@@ -0,0 +1,24 @@
+package internship.week13.Banking;
+
+import static org.junit.Assert.fail;
+
+/**
+ *
+ */
+public class BankAccountTest {
+	@org.junit.Test
+	public void deposit() throws Exception {
+        fail("This test has yet to be implemented");
+	}
+
+	@org.junit.Test
+	public void withdraw() throws Exception {
+		fail("This test has yet to be implemented");
+	}
+
+	@org.junit.Test
+	public void getBalance() throws Exception {
+		fail("This test has yet to be implemented");
+	}
+
+}
---
+++ src/internship/week13/Banking/Main.java
@@ -0,0 +1,8 @@
+package internship.week13.Banking;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("The application is running");
+	}
+}
